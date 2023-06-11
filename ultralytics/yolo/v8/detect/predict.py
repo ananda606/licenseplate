a63a759b2cb69ca3ffe21152fd06b9d54110b7ea
ananda606 <85835149+ananda606@users.noreply.github.com>
2023-06-11 09:37:28 +0700

    Update predict.py

--- ultralytics/yolo/v8/detect/predict.py
+++ ultralytics/yolo/v8/detect/predict.py
@@ -7,7 +7,21 @@ from ultralytics.yolo.engine.predictor import BasePredictor
 from ultralytics.yolo.utils import DEFAULT_CONFIG, ROOT, ops
 from ultralytics.yolo.utils.checks import check_imgsz
 from ultralytics.yolo.utils.plotting import Annotator, colors, save_one_box
+import cv2
+import easyocr
+reader=easyocr.Reader(['en'],gpu=True)
+def ocr_image(img,coordinates):
+  x,y,w,h = int(coordinates[0]),int(coordinates[1]),int(coordinates[2]),int(coordinates[3])
+  img=img[y:h,x:w]
+  gray=cv2.cvtcolor(img,cv2.COLOR_BGR2GRAY)
+  result=reader.readtext(gray)
 
+  text=''
+  for res in result:
+    if len(result)==3:
+      text=res[1]
+    if len(result)>3 and len(res[1])>6 and res[2]>0.2:
+      text=res[1]
 
 class DetectionPredictor(BasePredictor):
 
@@ -72,6 +86,8 @@ class DetectionPredictor(BasePredictor):
                 c = int(cls)  # integer class
                 label = None if self.args.hide_labels else (
                     self.model.names[c] if self.args.hide_conf else f'{self.model.names[c]} {conf:.2f}')
+                text_ocr=ocr_image(im0,xyxy)
+                label=text_ocr
                 self.annotator.box_label(xyxy, label, color=colors(c, True))
             if self.args.save_crop:
                 imc = im0.copy()
